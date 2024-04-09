051af3e7f7faaab8e76c468e775db9f1b6fcee22
vectorch9 <bluetifulc@gmail.com>
2024-04-09 15:33:58 +0900

    refactor: 사용하지 않는 메서드 제거

--- src/main/java/webserver/response/FileView.java
+++ src/main/java/webserver/response/FileView.java
@@ -25,13 +25,4 @@ public class FileView {
         return Optional.empty();
     }
 
-    private MediaType extractMediaType(String path) {
-        int index = path.lastIndexOf(".");
-        if (index == -1) {
-            return MediaType.OCTET_STREAM;
-        }
-        String extension = path.substring(index + 1).toLowerCase();
-        return MediaType.fromExtension(extension);
-    }
-
 }
